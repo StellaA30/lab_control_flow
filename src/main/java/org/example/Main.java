30d2da2a1fa822fb677b1913d2a152e26b66c1cd
Stella Annor <stella.annor@icloud.com>
2023-03-24 07:45:24 +0000

    added comments to code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello, let's play a guessing game");
+        System.out.println("Hello, let's play a guessing game");
         int secret_number = 7;
+//        user input
         Scanner sc = new Scanner(System.in);
         int guess = sc.nextInt();
 
+//        conditionals to check if user guessed correctly
         if(guess < secret_number){
             System.out.println("Incorrect, your guess is too low, try again!");
         } else if(guess == secret_number){
