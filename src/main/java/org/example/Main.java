445e8293562aace450623247d0be9acb2b48b496
Stella Annor <stella.annor@icloud.com>
2023-03-24 07:37:54 +0000

    added folder for guessing game lab

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello, let's play a guessing game");
+        int secret_number = 7;
+        Scanner sc = new Scanner(System.in);
+        int guess = sc.nextInt();
+
+        if(guess < secret_number){
+            System.out.println("Incorrect, your guess is too low, try again!");
+        } else if(guess == secret_number){
+            System.out.println("You guessed correctly!");
+        } else {
+            System.out.println("Incorrect, your guess is too high, try again!");
+        }
+
+    }
+}
